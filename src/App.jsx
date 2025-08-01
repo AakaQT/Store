e051277ebda96cb7c60214685f4bfebc62a5a33d
Aakarshan Jorge <aaka022009@gmail.com>
2025-08-01 14:55:19 +0200

    Made the most part of the project

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,17 @@ import Navbar from './components/Navbar.jsx'
 import { useState, useEffect } from "react"
 import ProductCard from "./components/ProductCard.jsx"
 
+
 const App = () => {
   const [search, setSearch] = useState("");
   const [productList, setProductList] = useState([]);
 
   const STORE_API_DATA = () => fetch("https://fakestoreapi.com/products")
     .then(response => response.json())
-    .then(data => {
-      setProductList(data);
-      console.log(data);
-    })
+    .then(data => setProductList(data))
     .catch(error => {
       console.error(`Error fetching products: ${error}`);
-    })
+    });
   
   
   useEffect(() => {
@@ -27,6 +25,7 @@ const App = () => {
       <Navbar search={search} setSearch={setSearch} />
 
       <h1 className="text-4xl">Products</h1>
+      
       <div className="flex flex-wrap p-[20px]">
         {productList.map(product => (
           <ProductCard key={product.id} name={product.title} img={product.image} price={product.price} rating={product.rating.rate} />
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,8 +5,10 @@ const ProductCard = ({ name, img, price, rating }) => {
     <div className="w-[400px] mb-[20px] bg-white rounded-[10px] mr-[30px] p-[10px]">
         <img src={img} alt={name} className="w-[250px] ml-auto mr-auto" />
         <h1>{name}</h1>
-        <span>{price}€</span>
+        <span className="mr-[10px]">{price}€</span>
         <span>{rating}</span>
+        <button className="block w-[350px] bg-blue-400 mr-auto ml-auto h-[40px] rounded-[10px] cursor-pointer hover:bg-blue-500">Buy now</button>
+        <button className="block w-[350px] bg-gray-200 mr-auto ml-auto mt-[10px] h-[40px] rounded-[10px] cursor-pointer hover:bg-gray-300">Add to cart</button>
     </div>
   )
 }
