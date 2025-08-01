2781d68452a28a299b6afa6c53949d2b4ba60412
Aakarshan Jorge <aaka022009@gmail.com>
2025-08-01 14:29:15 +0200

    added the API to the project and display it. Making the indivual pages now

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,32 @@ import ProductCard from "./components/ProductCard.jsx"
 const App = () => {
   const [search, setSearch] = useState("");
   const [productList, setProductList] = useState([]);
-  let productData;
 
   const STORE_API_DATA = () => fetch("https://fakestoreapi.com/products")
     .then(response => response.json())
-    .then(data => setProductList(data))
+    .then(data => {
+      setProductList(data);
+      console.log(data);
+    })
     .catch(error => {
       console.error(`Error fetching products: ${error}`);
     })
   
   
-    useEffect(() => {
+  useEffect(() => {
     STORE_API_DATA();
   }, [])
+  
   return (
-    <div>
+    <div className="bg-gray-100">
       <Navbar search={search} setSearch={setSearch} />
 
-      <h1>Products</h1>
-      {productList.map(product => (
-        <ProductCard key={product.id} name={product.title} />
-      ))}
+      <h1 className="text-4xl">Products</h1>
+      <div className="flex flex-wrap p-[20px]">
+        {productList.map(product => (
+          <ProductCard key={product.id} name={product.title} img={product.image} price={product.price} rating={product.rating.rate} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({ name, img, price, rating }) => {
   return (
-    <div>
-        
+    <div className="w-[400px] mb-[20px] bg-white rounded-[10px] mr-[30px] p-[10px]">
+        <img src={img} alt={name} className="w-[250px] ml-auto mr-auto" />
+        <h1>{name}</h1>
+        <span>{price}€</span>
+        <span>{rating}</span>
     </div>
   )
 }
